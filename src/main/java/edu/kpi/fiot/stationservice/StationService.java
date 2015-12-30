fcb69c9e57a3c404de59649666f81205232496e3
JackKarichkovskiy <karichkovsky@yandex.com>
2015-12-30 22:16:00 +0200

    added PostConstruct method for building hibernate factory

--- src/main/java/edu/kpi/fiot/stationservice/StationService.java
+++ src/main/java/edu/kpi/fiot/stationservice/StationService.java
@@ -1,5 +1,6 @@
 package edu.kpi.fiot.stationservice;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -9,8 +10,13 @@ import edu.kpi.fiot.stationservice.service.dao.jpa.hibernate.HibernateService;
 @ApplicationPath("webapi")
 public class StationService extends Application{
 	
+	@PostConstruct
+	public void init(){
+		HibernateService.getInstance();
+	}
+	
 	@PreDestroy
-	public void preDestroy(){
+	public void destroy(){
 		HibernateService.getInstance().destroy();
 	}
 }
